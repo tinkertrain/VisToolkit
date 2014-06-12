7f089d7452eaf39fa8629bb608ae7b8a98ba89a8
Juan Pablo <tinkertrain@gmail.com>
2014-06-12 13:54:21 -0500

    Continued work on the module

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,15 @@
-/* global atomic, nv, d3, _ */
 (function() {
   'use strict';
 
-  var parsedData = function(facet, allValues) {
+  var tokens = {};
+  var $userQuery = $('#userQuery');
+  var facetSelect = document.getElementById('facetSelect');
+  var query;
+  var promise = {};
+  var chart;
+  var parsedFacets;
+
+  var parseNVD3Data = function(facet, allValues) {
     return [{
       values: allValues,
       key: facet,
@@ -10,49 +17,111 @@
     }];
   };
 
-  atomic.get('/data/facets.type.json')
-    .success(function(data) {
-      var facet = data.items.label;
-      var items = data.items[0].items;
-      var allValues = [];
-
-      _.forEach(items, function(item) {
-        allValues.push({
-          label: item.value,
-          value: item.freq
-        });
+  var parseAllFacets = function(data) {
+    var freq;
+    var parsedFacets = _.map(data.items, function(item) {
+      freq = 0;
+      _.forEach(item.items, function(it) {
+        freq += it.freq;
       });
-      console.log(nv);
-      nv.addGraph(function() {
-        var chart = nv.models.discreteBarChart()
-          .x(function(d) {
-            return d.label;
-          })
-          .y(function(d) {
-            return d.value;
-          })
-          .tooltips(false)
-          .showValues(true)
-          .transitionDuration(350);
-
-        chart.xAxis
-          .axisLabel('Type');
-
-        chart.yAxis
-          .axisLabel('Frequency')
-          .tickFormat(d3.format('g'));
-
-        d3.select('#chart svg')
-          .datum(parsedData(facet, allValues))
-          .transition().duration(500)
-          .call(chart);
-
-        nv.utils.windowResize(chart.update);
-
-        return chart;
-      });
-    })
-    .error(function(data) {
-      console.log(data);
+      return {
+        value: freq,
+        label: item.label
+      };
     });
+
+    return parsedFacets;
+  };
+
+  var parseOneFacet = function(data) {
+    var parsedFacet = _.map(data.items, function(item) {
+      return {
+        value: item.freq,
+        label: item.value
+      };
+    });
+
+    return parsedFacet.length > 0 ? parsedFacet : [{value: 0}];
+  };
+
+  promise.getAllFacets = webServices.getAllFacets();
+  promise.getAllFacets.success(function(data) {
+    PubSub.publish('facets.getAll', data);
+  });
+
+  $(facetSelect).on('change', function(e) {
+    e.preventDefault();
+    query = facetSelect.options[facetSelect.selectedIndex].text.toLowerCase().replace(' ', '_');
+    $userQuery.text(facetSelect.options[facetSelect.selectedIndex].text);
+    if(query !== 'all_facets') {
+      promise.getFacet = webServices.getFacet(query);
+      promise.getFacet.success(function(data) {
+        PubSub.publish('facets.getOne', data);
+      });
+    }
+    else {
+      d3.select('#chart svg')
+        .datum(parseNVD3Data('All facets', parsedFacets))
+        .transition()
+        .duration(500)
+        .call(chart);
+    }
+  });
+
+  var graphAllFacets = function(msg, data) {
+    parsedFacets = parseAllFacets(data);
+
+    // Populate the select with the facets
+    var i = 1;
+    _.forEach(parsedFacets, function(facet) {
+      facetSelect.options.add( new Option(facet.label, i++));
+    });
+
+    nv.addGraph(function() {
+      chart = nv.models.discreteBarChart()
+        .x(function(d) {
+          return d.label;
+        })
+        .y(function(d) {
+          return d.value;
+        })
+        .staggerLabels(true)
+        .tooltips(true)
+        // .showValues(true)
+        .transitionDuration(350);
+
+      chart.xAxis
+        .axisLabel(facetSelect.options[facetSelect.selectedIndex].text);
+
+      chart.yAxis
+        .axisLabel('Frequency')
+        .tickFormat(d3.format('g'));
+
+      d3.select('#chart svg')
+        .datum(parseNVD3Data('All facets', parsedFacets))
+        .transition()
+        .duration(500)
+        .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+  };
+  var graphOneFacet = function(msg, data) {
+    var parsedFacet = parseOneFacet(data);
+
+    chart.xAxis
+      .axisLabel(facetSelect.options[facetSelect.selectedIndex].text);
+
+    d3.select('#chart svg')
+      .datum(parseNVD3Data(facetSelect.options[facetSelect.selectedIndex].text, parsedFacet))
+      .transition()
+      .duration(500)
+      .call(chart);
+  };
+
+  tokens.allFacets = PubSub.subscribe( 'facets.getAll', graphAllFacets );
+  tokens.facet = PubSub.subscribe( 'facets.getOne', graphOneFacet );
+
 })();
